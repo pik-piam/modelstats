0f79e234589679e19bfa3aa35e0ad701b85d4688
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-03-25 00:43:07 +0100

    added a new option: now users can run 'rs2 -cr' (current runs) and get information on their running jobs no matter where they are

--- R/promptAndRun.R
+++ R/promptAndRun.R
@@ -6,6 +6,10 @@ promptAndRun<-function(mydir=".") {
     loopRuns(choose_folder("."))
   } else if (mydir=="-f") {
     loopRuns(dir())
+  } else if (mydir=="-cr") {
+    myruns<-system(paste0("squeue -u ",Sys.info()[["user"]]," -h -o '%Z'"),intern=TRUE)
+    options(width=150)
+    getRunStatus(myruns)
   } else {
     loopRuns(mydir)
   }
