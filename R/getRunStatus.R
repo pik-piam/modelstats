9a5cfcd60c93329b4117074611bfc0a5995340f8
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-03-25 19:31:53 +0100

    bugfix coupled runs

--- R/getRunStatus.R
+++ R/getRunStatus.R
@@ -133,7 +133,7 @@ getRunStatus<-function(mydir=dir(),sort="nf"){
     } # END Conv
     
     # Calib Iter
-    if (file.exists(logtxt) & grepl("Calib",out[i,"RunType"])) {
+    if (!is.null(out[i,"RunType"])) if (file.exists(logtxt) & grepl("Calib",out[i,"RunType"])) {
       calibiter <- length(suppressWarnings(system(paste0("grep 'CES calibration iteration' ",logtxt),intern=TRUE)))
       if (calibiter>0) out[i,"Iter"]<-paste0(out[i,"Iter"]," ","Clb: ",calibiter)
       if (!is.null(out[i,"Conv"])) if (out[i,"Conv"]=="converged" & length(system(paste0("find ",ii," -name 'input_*.gdx'"),intern = TRUE))>10) out[i,"Conv"]<-"Clb_converged"
--- R/promptAndRun.R
+++ R/promptAndRun.R
@@ -1,5 +1,5 @@
-promptAndRun<-function(mydir=".") {
-  
+promptAndRun<-function(mydir=".",user=NULL) {
+  if (is.null(user)) user <- Sys.info()[["user"]]
   if (mydir==".") {
     loopRuns(".")
   } else if (mydir=="") {
@@ -7,9 +7,9 @@ promptAndRun<-function(mydir=".") {
   } else if (mydir=="-f") {
     loopRuns(dir())
   } else if (mydir=="-cr") {
-    myruns<-system(paste0("squeue -u ",Sys.info()[["user"]]," -h -o '%Z'"),intern=TRUE)
-    runnames<-system(paste0("squeue -u ",Sys.info()[["user"]]," -h -o '%j'"),intern=TRUE)
-    coupled<-NULL
+    myruns<-system(paste0("squeue -u ",user," -h -o '%Z'"),intern=TRUE)
+    runnames<-system(paste0("squeue -u ",user," -h -o '%j'"),intern=TRUE)
+    coupled<-rem<-NULL
     for (i in 1:length(runnames)) {
       if (grepl(runnames[[i]],myruns[[i]])) {
         next
@@ -19,16 +19,24 @@ promptAndRun<-function(mydir=".") {
         coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-3"))
         coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-4"))
         coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-5"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-6"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-7"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-8"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-9"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-10"))
+        rem <- c(rem,i)
       }
     }
-    myruns <- c(myruns,coupled)
+    myruns<-myruns[-rem] # remove coupled parent-job
+    myruns <- c(myruns,coupled) # add coupled paths
+    myruns<-myruns[file.exists(myruns)] # keep only existing paths
     
     options(width=150)
     getRunStatus(myruns)
-
+    
   } else {
     loopRuns(mydir)
   }
-
+  
 }
 
