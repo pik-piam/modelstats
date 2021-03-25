729358777d813f6f6e487ad524900aa49e716e3f
giannou <11047746+giannou@users.noreply.github.com>
2021-03-25 09:17:04 +0100

    add coupled runs to the -cr option of rs2

--- R/promptAndRun.R
+++ R/promptAndRun.R
@@ -7,9 +7,25 @@ promptAndRun<-function(mydir=".") {
   } else if (mydir=="-f") {
     loopRuns(dir())
   } else if (mydir=="-cr") {
-    myruns<-system(paste0("squeue -u ",Sys.info()[["user"]]," -h -o '%Z'"),intern=TRUE)
+    myruns<-system(paste0("squeue -u ","hilaire"," -h -o '%Z'"),intern=TRUE)
+    runnames<-system(paste0("squeue -u ","hilaire"," -h -o '%j'"),intern=TRUE)
+    coupled<-NULL
+    for (i in 1:length(runnames)) {
+      if (grepl(runnames[[i]],myruns[[i]])) {
+        next
+      } else {
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-1"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-2"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-3"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-4"))
+        coupled<-c(coupled,paste0(myruns[[i]],"/output/",runnames[[i]],"-rem-5"))
+      }
+    }
+    myruns <- c(myruns,coupled)
+    
     options(width=150)
     getRunStatus(myruns)
+
   } else {
     loopRuns(mydir)
   }
