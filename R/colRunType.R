4e7092a75f9c008a55d0873d2d729082cc1e09f5
giannou <11047746+giannou@users.noreply.github.com>
2021-04-26 10:53:07 +0200

    MAgPIE in colRunType

--- R/colRunType.R
+++ R/colRunType.R
@@ -12,16 +12,23 @@ colRunType<-function(mydir="."){
   cfgf <- paste0(mydir,"/config.Rdata")
   fulllst <- paste0(mydir,"/full.lst")
   
+  out <- "NA"
   if (file.exists(cfgf)) {
     load(cfgf)
-    if (cfg[["model_name"]] == "MAgPIE") return("MAgPIE")
-    out <- cfg[["gms"]][["optimization"]]
-    if (cfg[["gms"]][["CES_parameters"]]=="calibrate") out<-paste0("Calib_",out)
+    if (cfg[["model_name"]] == "MAgPIE") {
+        out <- cfg[["gms"]][["optimization"]]
+    } else {
+        out <- cfg[["gms"]][["optimization"]]
+        if (cfg[["gms"]][["CES_parameters"]]=="calibrate") out<-paste0("Calib_",out)
+    }
   } else if (file.exists(fulllst)) {
-    out <- sub("         !! def = nash","",sub("^ .*.ion  ","",system(paste0("grep 'setGlobal optimization  ' ",fulllst),intern=TRUE)))
-    chck <- sub("       !! def = load","",sub("^ .*.ers  ","",system(paste0("grep 'setglobal CES_parameters  ' ",fulllst),intern=TRUE)))
-    if (chck=="calibrate") out <- paste0("Calib_",out)
+        out <- sub("         !! def = nash","",sub("^ .*.ion  ","",system(paste0("grep 'setGlobal optimization  ' ",fulllst),intern=TRUE)))
+        chck <- sub("       !! def = load","",sub("^ .*.ers  ","",system(paste0("grep 'setglobal CES_parameters  ' ",fulllst),intern=TRUE)))
+        if (chck=="calibrate") out <- paste0("Calib_",out)
   }
+
+    
+   
   return(out)
 
 }
